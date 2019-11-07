9151eb127ca6067f3026adc0a19ab89b1166a408
Maria Gabriela Barrios Cantera <barrios.cantera@gmail.com>
2019-11-07 16:46:47 +1100

    Updates to homework '#clase-3'

--- tareas/clase-3/tarea-clase-3-mio.js
+++ tareas/clase-3/tarea-clase-3-mio.js
@@ -7,33 +7,46 @@
 // Imprimir "Hola " y el nombre, " te llamás igual que mi ..."
 // Si no, simplemente imprimir "Hola " + nombre!
 
-/*const nombreDeUsuario = (prompt('Cual es tu nombre?',
-    'Ingrese su nombre')).toLowerCase();
-const miNombre = 'gabriela';
-const nombreMejorAmigo = 'florencia';
+const nombreDeUsuario = (prompt('Cual es tu nombre?') || '').toLowerCase();
 
-if (nombreDeUsuario === miNombre) {
+// null indica la ausencia de valor, esta vacio. Eso pasa si hacemos 'esc' en un prompt.
+// el string vacio se obtiene si hacemos 'enter' en un prompt sin ingresar datos.
+// un string vacio no es lo mismo que null.
+// 0, -0, false, '', null, undefined, NaN --> false
+// el || chequea que al menos 1 cosa es TRUE, si lo 1ro da false, te devuelve lo 2do (sin importar si es FALSE)
+//i.e. '' || false --> devuelve false
+
+const MI_NOMBRE = 'gabriela';
+const NOMBRE_MEJOR_AMIGO = 'florencia';
+
+if (nombreDeUsuario === MI_NOMBRE) {
   console.log(`Hola, Tocayo! Yo también me llamo ${nombreDeUsuario}`);
-} else if (nombreDeUsuario === nombreMejorAmigo) {
+} else if (nombreDeUsuario === NOMBRE_MEJOR_AMIGO) {
   console.log(`Hola, ${nombreDeUsuario}! Te llamas igual que mi mejor amiga`);
+} else if (nombreDeUsuario.trim().length === 0) {
+  console.log('No escribiste tu nombre!');
 } else {
   console.log(`Hola, ${nombreDeUsuario}!`);
-}*/
+}
 
 //Tarea 2:
 // Preguntar la edad del usuario
 // Hacerle saber si tiene más, menos ó la misma edad que nosotros.
 
-/*const edadUsuario = Number(prompt('Cual es tu edad?', 'Ingrese su edad'));
-const miEdad = 33;
+const MI_EDAD = 33;
+const edadUsuario = Number(prompt('Cual es tu edad?'));
 
-if (edadUsuario > miEdad) {
-  console.log('Sos mas grande que yo');
-} else if (edadUsuario === miEdad) {
+if (edadUsuario === 0) { // Number('') y Number(null) da 0
+  console.log('Tenes que ingresar tu edad');
+} else if (isNaN(edadUsuario)) { // si escribe cualquier otra cosa diferente de un integer
+  console.log('Ingresa por favor un numero en digitos');
+}else if (edadUsuario === MI_EDAD) {
   console.log('Tenemos la misma edad!');
-} else {
+} else if (edadUsuario > MI_EDAD) {
+  console.log('Sos mas grande que yo');
+} else if (edadUsuario < MI_EDAD) {
   console.log('Sos mas chico que yo');
-}*/
+}
 
 //Tarea 3:
 // Preguntarle al usuario si tiene documento, y que conteste con "si" o "no".
@@ -44,12 +57,12 @@ if (edadUsuario > miEdad) {
 // Si no entendemos la respuesta, le decimos que no entendimos la respuesta.
 // Punto bonus: SI, NO, Si, No, si, no.
 
-const tieneDocumento = prompt('Tenes el DNI?', 'SI / NO').toLowerCase();
+const tieneDocumento = (prompt('Tenes el DNI?', 'SI / NO') || '').toLowerCase();
 
 function bouncer(poseeDni) {
   if (poseeDni === 'si') {
     const edadUsuario = Number(prompt('cuantos años tenes?'));
-    if (edadUsuario >= 18)  {
+    if (edadUsuario >= 18) {
       console.log('Podes pasar!');
     } else {
       console.log('Sos menor, volvete a tu casa!');
