fb8dde0b0d7ca9d539f205788f5aa29d6c204160
Maria Gabriela Barrios Cantera <barrios.cantera@gmail.com>
2019-11-19 11:28:22 +1100

    refactoring task 5

--- tareas/clase-4/algoritmo-5.js
+++ tareas/clase-4/algoritmo-5.js
@@ -4,12 +4,12 @@ Desafío de programación #5: Calcula el resultado de la suma de los numeros del
  1 + 2 + 3 + ... + 10 = ? //hacerlo con un array y un bucle
  */
 
-function addArray(arr) {
+function addArrayFromOneToTen() {
   let result = 0;
-  for (let i = 0; i < arr.length; i++) {
-    result += arr[i];
+  for (let i = 1; i <= 10; i++) {
+    result += i;
   }
   console.log(result);
 }
 
-addArray([1,2,3,4,5,6,7,8,9,10]);
+addArrayFromOneToTen();
