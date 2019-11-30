dbdb8c3a8d075bd0d6521e6c77d6e24a87f5dd67
Maria Gabriela Barrios Cantera <barrios.cantera@gmail.com>
2019-11-30 18:10:16 +1100

    organising problems into a file
    organising the rest of the algorithms to solve into a file

---
+++ tareas/clase-4/algoritmo-30.js
@@ -0,0 +1,26 @@
+/*
+Desafío de programación #30. Create a function that will add two positive numbers of indefinite size. The numbers are received as strings and the result should be also provided as string.
+Desafío de programación #31. Create a function that will return the number of words in a text
+Desafío de programación #32. Create a function that will capitalize the first letter of each word in a text
+Desafío de programación #33. Calculate the sum of numbers received in a comma delimited string
+Desafío de programación #34. Create a function that will return an array with words inside a text
+Desafío de programación #35. Create a function to convert a CSV text to a “bi-dimensional” array
+Desafío de programación #36. Create a function that converts a string to an array of characters
+Desafío de programación #37. Create a function that will convert a string in an array containing the ASCII codes of each character
+Desafío de programación #38. Create a function that will convert an array containing ASCII codes in a string
+Desafío de programación #39. Implement the Caesar cypher
+Desafío de programación #40. Implement the bubble sort algorithm for an array of numbers
+Desafío de programación #41. Create a function to calculate the distance between two points defined by their x, y coordinates
+Desafío de programación #42. Create a function that will return a Boolean value indicating if two circles defined by center coordinates and radius are intersecting
+Desafío de programación #43. Create a function that will receive a bi-dimensional array as argument and a number and will extract as a unidimensional array the column specified by the number
+Desafío de programación #44. Create a function that will convert a string containing a binary number into a number
+Desafío de programación #45. Create a function to calculate the sum of all the numbers in a jagged array (array contains numbers or other arrays of numbers on an unlimited number of levels)
+Desafío de programación #46. Find the maximum number in a jagged array of numbers or array of numbers
+Desafío de programación #47. Deep copy a jagged array with numbers or other arrays in a new array
+Desafío de programación #48. Create a function to return the longest word(s) in a string
+Desafío de programación #49. Shuffle an array of strings
+Desafío de programación #50. Create a function that will receive n as argument and return an array of n unique random numbers from 1 to n.
+Desafío de programación #51. Find the frequency of characters inside a string. Return the result as an array of objects. Each object has 2 fields: character and number of occurrences.
+Desafío de programación #52. Calculate Fibonacci(500) with high precision (all decimals)
+Desafío de programación #53. Calculate 70! with high precision (all decimals)
+*/
